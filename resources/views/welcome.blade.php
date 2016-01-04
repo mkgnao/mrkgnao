5cc889ae8bf8bba863ba1adb7b166b00978e3d6e
ddyok <mkgnao@yandex.com>
2016-01-04 19:35:43 +0100

    add font files and basic css

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,8 +3,8 @@
     <head>
         <title>m r k g n a o</title>
 
-        <link href="http://mrkgnao.co/css/dr.css" rel="stylesheet" type="text/css">
-        <link href="http://mrkgnao.co/css/main.css" rel="stylesheet" type="text/css">
+        <link href="https://mrkgnao.co/css/dr.css" rel="stylesheet" type="text/css">
+        <link href="https://mrkgnao.co/css/main.css" rel="stylesheet" type="text/css">
 
     </head>
     <body>

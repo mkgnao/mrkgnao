b4d0da529886b21cbb0914fa51be01fc3c958f31
ddyok <mkgnao@yandex.com>
2016-01-04 22:32:11 +0100

    add font files and basic css

--- public/js/supports.js
+++ public/js/supports.js
@@ -1,9 +1,8 @@
 var supports = {};
 var style = document.body.style;
 
-export default {
-  flexbox: function() {
+
+var flexbox = function() {
     return supports.flexbox || (supports.flexbox = ('flexBasis' in style ||
         'msFlexAlign' in style || 'webkitBoxDirection' in style));
-  }
 };

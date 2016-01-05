8c258d4285c1fc889da6653ee41ae9f8546f854d
ddyok <mkgnao@yandex.com>
2016-01-05 12:34:20 +0100

    login stuff

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,20 +1,5 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="joyce" unselectable="on">
-        The coals were reddening.
-        Another slice of bread and butter: three, four: right. She didn't like her plate full. Right. He turned from the tray, lifted the kettle off the hob and set it sideways on the fire. It sat there, dull and squat, its spout stuck out. Cup of tea soon. Good. Mouth dry. The cat walked stiffly round a leg of the table with tail on high.
-        —Mkgnao!
-        —O, there you are, Mr Bloom said, turning from the fire.
-        The cat mewed in answer and stalked again stiffly round a leg of the table, mewing. Just how she stalks over my writingtable. Prr. Scratch my head. Prr.
-        Mr Bloom watched curiously, kindly the lithe black form. Clean to see: the gloss of her sleek hide, the white button under the butt of her tail, the green flashing eyes. He bent down to her, his hands on his knees.
-        —Milk for the pussens, he said.
-        —Mrkgnao! the cat cried.
-        They call them stupid. They understand what we say better than we understand them. She understands all she wants to. Vindictive too. Cruel. Her nature. Curious mice never squeal. Seem to like it. Wonder what I look like to her. Height of a tower? No, she can jump me.
-        —Afraid of the chickens she is, he said mockingly. Afraid of the chookchooks. I never saw such a stupid pussens as the pussens.
-        Cruel. Her nature. Curious mice never squeal. Seem to like it.
-        —Mrkrgnao! the cat said loudly.
-        She blinked up out of her avid shameclosing eyes, mewing plaintively and long, showing him her milkwhite teeth. He watched the dark eyeslits narrowing with greed till her eyes were green stones. Then he went to the dresser, took the jug Hanlon's milkman had just filled for him, poured warmbubbled milk on a saucer and set it slowly on the floor.
-        —Gurrhr! she cried, running to lap.
-    </div>
+    <div class="joyce" unselectable="on">m k g n a o</div>
 @endsection

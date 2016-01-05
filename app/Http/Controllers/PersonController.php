7f73873c0c39e92af46c3ea73bf7aa4dd0c55f5e
ddyok <mkgnao@yandex.com>
2016-01-05 07:04:39 +0100

    login stuff

--- app/Http/Controllers/PersonController.php
+++ app/Http/Controllers/PersonController.php
@@ -50,8 +50,8 @@ class PersonController extends Controller
             $auth = true;
         }
 
+        $json_string = json_encode($account, JSON_PRETTY_PRINT);
 
-        return view('/u/main', array('tw' => $account));
+        return view('/u/main', array('tw' => $json_string));
     }
-
 }

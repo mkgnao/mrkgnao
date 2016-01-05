7d6be03aec4e1412db54a7ec437f53adca503d4c
ddyok <mkgnao@yandex.com>
2016-01-05 01:45:47 +0100

    add font files and basic css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -89,8 +89,6 @@
 </header>
 <main class="HolyGrail-body">
     <article class="HolyGrail-content">
-        content:
-
         @yield('content')
     </article>
     <nav class="HolyGrail-nav u-textCenter">

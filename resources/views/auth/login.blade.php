e47f7b9f709967908de96c397bbf03b67887564a
ddyok <mkgnao@yandex.com>
2016-01-05 02:03:59 +0100

    add font files and basic css

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,43 +1,55 @@
 @extends('layouts.app')
 
 @section('content')
-    <form class=".HolyGrail-form" role="form" method="POST" action="{{ url('/login') }}">
+    <form role="form" method="POST" action="{{ url('/login') }}">
         {!! csrf_field() !!}
-        <div class="HolyGrail-form-email">
-            email
-            <input type="email" name="email" value="{{ old('email') }}">
 
-            @if ($errors->has('email'))
-                {{ $errors->first('email') }}
-            @endif
+        <div class="Grid Grid--guttersLg Grid--full med-Grid--fit">
+
+            <div class="Grid-cell">
+                <div class="InputAddOn">
+                    <span class="InputAddOn-item">email</span>
+
+                    <input type="email" class="InputAddOn-field" name="email" value="{{ old('email') }}">
+
+                    @if ($errors->has('email'))
+                        {{ $errors->first('email') }}
+                    @endif
+                </div>
+            </div>
+
+            <div class="Grid-cell">
+                <div class="InputAddOn">
+                    <span class="InputAddOn-item">password</span>
+                    <input type="password" class="InputAddOn-field" name="password">
+
+                    @if ($errors->has('password'))
+                        {{ $errors->first('password') }}
+                    @endif
+                </div>
+            </div>
+
+            <div class="Grid-cell">
+                <div class="InputAddOn">
+                    remember
+                    <input type="checkbox" class="InputAddOn-item" name="remember">
+                </div>
+            </div>
+
+            <div class="Grid-cell">
+                <div class="InputAddOn">
+                    <button class="InputAddOn-item" type="submit">
+                        submit
+                    </button>
+                </div>
+            </div>
+
+            <div class="Grid-cell">
+                <div class="InputAddOn">
+                    <a href="{{ url('/password/reset') }}">?</a>
+                </div>
+            </div>
 
         </div>
-
-        <div>
-            password
-            <input type="password" name="password">
-
-            @if ($errors->has('password'))
-                {{ $errors->first('password') }}
-            @endif
-
-        </div>
-
-        <div>
-            remember
-            <input type="checkbox" name="remember">
-        </div>
-
-        <div>
-            <button type="submit">
-                submit
-            </button>
-
-        </div>
-
-        <div>
-            <a href="{{ url('/password/reset') }}">?</a>
-        </div>
-
     </form>
 @endsection

26e065f37884dc9ade14585ae5fb51b421f15cac
ddyok <mkgnao@yandex.com>
2016-01-05 12:19:19 +0100

    login stuff

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,6 +2,33 @@
 
 @section('content')
     <div class="joyce" unselectable="on">
-        these heavy sands are language tide and wind have silted here
+        Mr Leopold Bloom ate with relish the inner organs of beasts and fowls. He liked thick giblet soup, nutty gizzards, a stuffed roast heart, liverslices fried with crustcrumbs, fried hencods' roes. Most of all he liked grilled mutton kidneys which gave to his palate a fine tang of faintly scented urine.
+        Kidneys were in his mind as he moved about the kitchen softly, righting her breakfast things on the humpy tray. Gelid light and air were in the kitchen but out of doors gentle summer morning everywhere. Made him feel a bit peckish.
+        The coals were reddening.
+        Another slice of bread and butter: three, four: right. She didn't like her plate full. Right. He turned from the tray, lifted the kettle off the hob and set it sideways on the fire. It sat there, dull and squat, its spout stuck out. Cup of tea soon. Good. Mouth dry. The cat walked stiffly round a leg of the table with tail on high.
+        —Mkgnao!
+        —O, there you are, Mr Bloom said, turning from the fire.
+        The cat mewed in answer and stalked again stiffly round a leg of the table, mewing. Just how she stalks over my writingtable. Prr. Scratch my head. Prr.
+        Mr Bloom watched curiously, kindly the lithe black form. Clean to see: the gloss of her sleek hide, the white button under the butt of her tail, the green flashing eyes. He bent down to her, his hands on his knees.
+        —Milk for the pussens, he said.
+        —Mrkgnao! the cat cried.
+        They call them stupid. They understand what we say better than we understand them. She understands all she wants to. Vindictive too. Cruel. Her nature. Curious mice never squeal. Seem to like it. Wonder what I look like to her. Height of a tower? No, she can jump me.
+        —Afraid of the chickens she is, he said mockingly. Afraid of the chookchooks. I never saw such a stupid pussens as the pussens.
+        Cruel. Her nature. Curious mice never squeal. Seem to like it.
+        —Mrkrgnao! the cat said loudly.
+        She blinked up out of her avid shameclosing eyes, mewing plaintively and long, showing him her milkwhite teeth. He watched the dark eyeslits narrowing with greed till her eyes were green stones. Then he went to the dresser, took the jug Hanlon's milkman had just filled for him, poured warmbubbled milk on a saucer and set it slowly on the floor.
+        —Gurrhr! she cried, running to lap.
+        He watched the bristles shining wirily in the weak light as she tipped three times and licked lightly. Wonder is it true if you clip them they can't mouse after. Why? They shine in the dark, perhaps, the tips. Or kind of feelers in the dark, perhaps.
+        He listened to her licking lap. Ham and eggs, no. No good eggs with this drouth. Want pure fresh water. Thursday: not a good day either for a mutton kidney at Buckley's. Fried with butter, a shake of pepper. Better a pork kidney at Dlugacz's. While the kettle is boiling. She lapped slower, then licking the saucer clean. Why are their tongues so rough? To lap better, all porous holes. Nothing she can eat? He glanced round him. No.
+        On quietly creaky boots he went up the staircase to the hall, paused by the bedroom door. She might like something tasty. Thin bread and butter she likes in the morning. Still perhaps: once in a way.
+        He said softly in the bare hall:
+        —I'm going round the corner. Be back in a minute.
+        And when he had heard his voice say it he added:
+        —You don't want anything for breakfast?
+        A sleepy soft grunt answered:
+        —Mn.
+        No. She didn't want anything. He heard then a warm heavy sigh, softer, as she turned over and the loose brass quoits of the bedstead jingled. Must get those settled really. Pity. All the way from Gibraltar. Forgotten any little Spanish she knew. Wonder what her father gave for it. Old style. Ah yes! of course. Bought it at the governor's auction. Got a short knock. Hard as nails at a bargain, old Tweedy. Yes, sir. At Plevna that was. I rose from the ranks, sir, and I'm proud of it. Still he had brains enough to make that corner in stamps. Now that was farseeing.
+        His hand took his hat from the peg over his initialled heavy overcoat and his lost property office secondhand waterproof. Stamps: stickyback pictures. Daresay lots of officers are in the swim too. Course they do. The sweated legend in the crown of his hat told him mutely: Plasto's high grade ha. He peeped quickly inside the leather headband. White slip of paper. Quite safe.
+        On the doorstep he felt in his hip pocket for the latchkey. Not there. In the trousers I left off. Must get it. Potato I have. Creaky wardrobe. No use disturbing her. She turned over sleepily that time. He pulled the halldoor to after him very quietly, more, till the footleaf dropped gently over the threshold, a limp lid. Looked shut. All right till I come back anyhow.
     </div>
 @endsection

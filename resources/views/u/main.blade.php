e79f4f35f35bdc6b009b09187276d8a5adeaee38
ddyok <mkgnao@yandex.com>
2016-01-05 16:19:27 +0100

    tw stuff

--- resources/views/u/main.blade.php
+++ resources/views/u/main.blade.php
@@ -7,14 +7,10 @@
         <div class="Grid Grid--guttersLg Grid--full med-Grid--fit">
             <div class="Grid-cell">
                 <div id="twcontent">
-
+                    {{ dd(get_defined_vars()['__data']) }}
                 </div>
             </div>
         </div>
 </div>
-
-    <script type="text/javascript">
-        console.log(tw);
-    </script>
 
 @endsection

3aeca3d4dcbe63bb215b50368a22e2ded08619f0
ddyok <mkgnao@yandex.com>
2016-01-05 16:27:38 +0100

    tw stuff

--- config/javascript.php
+++ config/javascript.php
@@ -13,7 +13,7 @@ return [
     | Example: 'footer' or ['footer', 'bottom']
     |
     */
-    'bind_js_vars_to_this_view' => 'main',
+    'bind_js_vars_to_this_view' => '/u/main',
 
     /*
     |--------------------------------------------------------------------------
@@ -26,5 +26,4 @@ return [
     |
     */
     'js_namespace' => 'window'
-
 ];

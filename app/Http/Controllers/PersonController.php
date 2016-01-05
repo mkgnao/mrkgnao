08b100c5c03c304c5e0b7abe3585d2337f83a309
ddyok <mkgnao@yandex.com>
2016-01-05 18:20:57 +0100

    tw stuff

--- app/Http/Controllers/PersonController.php
+++ app/Http/Controllers/PersonController.php
@@ -50,7 +50,7 @@ class PersonController extends Controller
             $auth = true;
         }
 
-        Laracasts\Utilities\JavaScript::put([
+        \Laracasts\Utilities\JavaScript::put([
             'tw' => $value
         ]);
 

9d80d8e34098ac0204d96b879421be870722db4e
ddyok <mkgnao@yandex.com>
2016-01-05 12:45:54 +0100

    login stuff

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,43 +1,54 @@
 @extends('layouts.app')
 
 @section('content')
-    <form role="form" method="POST" action="{{ url('/login') }}">
-        {!! csrf_field() !!}
-
+    @if(url('') != 'https://mrkgnao.co')
         <div class="Container">
             <div class="Grid Grid--guttersLg Grid--full med-Grid--fit">
-
                 <div class="Grid-cell">
-                    <div class="InputAddOn">
-                        <span class="InputAddOn-item">email</span>
-
-                        <input type="email" class="InputAddOn-field" name="email" value="{{ old('email') }}">
-
-                        @if ($errors->has('email'))
-                            {{ $errors->first('email') }}
-                        @endif
-                    </div>
-                    <div class="InputAddOn">
-                        <span class="InputAddOn-item">password</span>
-                        <input type="password" class="InputAddOn-field" name="password">
-
-                        @if ($errors->has('password'))
-                            {{ $errors->first('password') }}
-                        @endif
-                    </div>
-                    <!--div class="InputAddOn">
-                        <input type="checkbox" class="InputAddOn-item" name="remember">
-                    </div-->
-                    <div class="InputAddOn">
-                        <button class="InputAddOn-item" type="submit">
-                            login
-                        </button>
-                    </div>
-                    <!--div class="InputAddOn">
-                        <a class="InputAddOn-item" href="{{ url('/password/reset') }}">reset</a>
-                    </div-->
+                    you are not allowed
                 </div>
             </div>
         </div>
-    </form>
+    @else
+        <form role="form" method="POST" action="{{ url('/login') }}">
+            {!! csrf_field() !!}
+
+            <div class="Container">
+                <div class="Grid Grid--guttersLg Grid--full med-Grid--fit">
+
+                    <div class="Grid-cell">
+                        <div class="InputAddOn">
+                            <span class="InputAddOn-item">email</span>
+
+                            <input type="email" class="InputAddOn-field" name="email"
+                                   value="{{ old('email') }}">
+
+                            @if ($errors->has('email'))
+                                {{ $errors->first('email') }}
+                            @endif
+                        </div>
+                        <div class="InputAddOn">
+                            <span class="InputAddOn-item">password</span>
+                            <input type="password" class="InputAddOn-field" name="password">
+
+                            @if ($errors->has('password'))
+                                {{ $errors->first('password') }}
+                            @endif
+                        </div>
+                        <!--div class="InputAddOn">
+                            <input type="checkbox" class="InputAddOn-item" name="remember">
+                        </div-->
+                        <div class="InputAddOn">
+                            <button class="InputAddOn-item" type="submit">
+                                login
+                            </button>
+                        </div>
+                        <!--div class="InputAddOn">
+                        <a class="InputAddOn-item" href="{{ url('/password/reset') }}">reset</a>
+                    </div-->
+                    </div>
+                </div>
+            </div>
+        </form>
+    @endif
 @endsection

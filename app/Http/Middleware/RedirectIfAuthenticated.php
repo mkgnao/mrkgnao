1c4358e1b72a427f631320047fdb6c9f2e82c3a5
ddyok <mkgnao@yandex.com>
2016-01-06 19:49:50 +0100

    tw stuff

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -18,7 +18,7 @@ class RedirectIfAuthenticated
     public function handle($request, Closure $next, $guard = null)
     {
         if (Auth::guard($guard)->check()) {
-            return redirect('/u/' . Auth::id() . '/main');
+            return redirect()->intended('/u/' . Auth::id() . '/main');
         }
 
         return $next($request);

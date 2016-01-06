e93d016f2efb3ad3090804bf37ae7e06903c1e87
ddyok <mkgnao@yandex.com>
2016-01-06 17:51:24 +0100

    tw stuff

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -42,15 +42,6 @@ Route::group(['middleware' => ['web']], function () {
     //
 });
 
-
-
-Route::group(['middleware' => 'web'], function () {
-    Route::auth();
-
-    //Route::get('/home', 'HomeController@index');
-    Route::get('/u/main', 'PersonController@index');
-});
-
 Route::group(['middleware' => 'web'], function () {
     Route::auth();
 

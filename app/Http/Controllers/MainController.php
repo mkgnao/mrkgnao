d0001dee9d4b791d2e985abc1fe898c5e205ad93
ddyok <mkgnao@yandex.com>
2016-01-06 18:26:02 +0100

    tw stuff

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -71,8 +71,7 @@ class MainController extends Controller
         $tw_coupling_user = $tw_coupling->find($user_id);
 
         if ($tw_coupling_user->tw_id < 0) {
-            $tw_coupling_user->tw_id = $this->tw_me->id;
-            $tw_coupling_user->save();
+            $tw_coupling_user->update('tw_id', Array($this->tw_me->id));
         }
     }
 

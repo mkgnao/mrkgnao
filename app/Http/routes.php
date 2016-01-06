8c541f7b656761b49c646a6831525c4d04838a7b
ddyok <mkgnao@yandex.com>
2016-01-06 19:35:03 +0100

    tw stuff

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -19,10 +19,6 @@ Route::get('/css', function () {
     //css
 });
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::get('partials.scriptsend', function() {
     return View::make('partials/scriptsend');
 });
@@ -41,8 +37,15 @@ Route::get('partials.scriptsend', function() {
 Route::group(['middleware' => 'web'], function () {
     Route::auth();
 
+    Route::get('/', function() {
+        Redirect::to('/u/' . \Auth()->id . '/main', array('as' => '/u/main', 'uses' => 'MainController@index'));
+    });
+
     Route::get('/u/' . '{id}' . '/main', array('as' => '/u/main', 'uses' => 'MainController@index'));
 
     Route::get('/u/' . '{id}' . '/tasklist', array('as' => '/u/tasklist', 'uses' => 'TaskListController@index'));
 });
 
+Route::get('/', function () {
+    return view('welcome');
+});

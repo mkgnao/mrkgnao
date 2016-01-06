3e9e3506f4dfb7763752074882b099c2540cfc34
ddyok <mkgnao@yandex.com>
2016-01-06 19:47:12 +0100

    tw stuff

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,16 +11,22 @@
 |
 */
 
-Route::get('/fonts', function () {
-    //fonts
-});
-
-Route::get('/css', function () {
-    //css
-});
-
 Route::get('partials.scriptsend', function() {
     return View::make('partials/scriptsend');
+});
+
+Route::group(['middleware' != 'web'], function () {
+    Route::get('/fonts', function () {
+        //fonts
+    });
+
+    Route::get('/css', function () {
+        //css
+    });
+
+    Route::get('/', function () {
+        return view('welcome');
+    });
 });
 
 /*
@@ -44,8 +50,4 @@ Route::group(['middleware' => 'web'], function () {
     Route::get('/u/' . '{id}' . '/main', array('as' => '/u/main', 'uses' => 'MainController@index'));
 
     Route::get('/u/' . '{id}' . '/tasklist', array('as' => '/u/tasklist', 'uses' => 'TaskListController@index'));
-});
-
-Route::get('/', function () {
-    return view('welcome');
 });

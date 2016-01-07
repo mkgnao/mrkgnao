b4c0b5fc64439cd3bc574cdbf03a558af5a5ed7e
ddyok <mkgnao@yandex.com>
2016-01-07 06:37:25 +0100

    tw stuff

--- resources/views/u/main.blade.php
+++ resources/views/u/main.blade.php
@@ -3,6 +3,20 @@
 @section('content')
 
     <div class="Container">
+        <div class="Grid Grid--guttersLg Grid--full med-Grid--fit">
+            <div class="Grid-cell">
+                <a href="/u/{{ $c->user_id_pad }}/main">main</a>
+            </div>
+            <div class="Grid-cell">
+                <a href="/u/{{ $c->user_id_pad }}/company">organizations</a>
+            </div>
+            <div class="Grid-cell">
+                <a href="/u/{{ $c->user_id_pad }}/people">people</a>
+            </div>
+            <div class="Grid-cell">
+                <a href="/u/{{ $c->user_id_pad }}/projects">projects</a>
+            </div>
+        </div>
 
         <div class="Grid Grid--guttersLg Grid--full med-Grid--fit">
             <div class="Grid-cell">

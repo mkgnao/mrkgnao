353298039ef33422cd16c6e30e390fd4532a4549
ddyok <mkgnao@yandex.com>
2016-01-07 06:43:23 +0100

    tw stuff

--- resources/views/u/main.blade.php
+++ resources/views/u/main.blade.php
@@ -2,7 +2,18 @@
 
 @section('content')
 
-    @yield('partials.topnav')
+    @include("partials.topnav")
+
+    <div class="Container">
+
+        <div class="Grid Grid--guttersLg Grid--full med-Grid--fit">
+            <div class="Grid-cell">
+                <div id="twcontent">
+
+                </div>
+            </div>
+        </div>
+    </div>
 
     <script>
         try {

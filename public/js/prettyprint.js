f37a47690dfbc21b5ca484fd52ca7f75794eca6f
ddyok <mkgnao@yandex.com>
2016-01-07 03:30:16 +0100

    tw stuff

--- public/js/prettyprint.js
+++ public/js/prettyprint.js
@@ -769,9 +769,7 @@ mkgnaoNs.prettyPrint = (function () {
                 td: {
                     padding: '1px',
                     fontSize: '16px',
-                    //border: '1px solid silver',
-                    border: '1px',
-                    borderColor: 'B0B0B0',
+                    border: '1px solid #B0B0B0',
                     verticalAlign: 'top',
                     whiteSpace: 'nowrap',
                     opacity: 0.78

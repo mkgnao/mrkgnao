06b86454b7219afb27ae650a06c878b6aca5c2be
ddyok <mkgnao@yandex.com>
2016-01-07 03:13:58 +0100

    tw stuff

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -27,14 +27,14 @@
                         @endif
                     </div>
                     <div class="InputAddOn">
-                        <div class="InputAddOn-item-checkbox">
-                            <input type="checkbox" class="InputAddOn-item-checkbox" name="remember">
-                        </div>
-                    </div>
-                    <div class="InputAddOn">
                         <button class="InputAddOn-button-login" type="submit">
                             login
                         </button>
+                    </div>
+                    <div class="InputAddOn">
+                        <div class="InputAddOn-item-checkbox">
+                            <input type="checkbox" class="InputAddOn-item-checkbox" name="remember">
+                        </div>
                     </div>
                     <!--div class="InputAddOn">
                         <a class="InputAddOn-item-link" href="{{ url('/password/reset') }}">reset</a>

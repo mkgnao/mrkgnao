7b75432113985122956a25c07953dcebdea4d143
ddyok <mkgnao@yandex.com>
2016-01-07 04:05:37 +0100

    tw stuff

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -59,4 +59,6 @@ Route::group(['middleware' => 'web'], function () {
     Route::get('/u/' . '{id}' . '/main', array('as' => '/u/main', 'uses' => 'MainController@index'));
 
     Route::get('/u/' . '{id}' . '/tasklist', array('as' => '/u/tasklist', 'uses' => 'TaskListController@index'));
+
+    Route::get('/u/' . '{id}' . '/company', array('as' => '/u/company', 'uses' => 'CompanyController@index'));
 });

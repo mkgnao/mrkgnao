e01cf383f8fee5a15b72bd05ac8b42a19d5f19bc
ddyok <mkgnao@yandex.com>
2016-01-07 06:33:03 +0100

    tw stuff

---
+++ resources/views/u/projects.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="Container">
+
+        <div class="Grid Grid--guttersLg Grid--full med-Grid--fit">
+            <div class="Grid-cell">
+                <div id="twcontent">
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script>
+        try {
+            //console.log(mkgnaoNs);
+
+            if (mkgnaoNs.tw_projects) {
+                mkgnaoNs.twAddJson(mkgnaoNs.tw_projects, "twcontent");
+            } else {
+                var e = document.getElementById("twcontent");
+                var n = document.createElement("div");
+                n.id = "twnotfound";
+                n.innerHTML = "not on teamwork";
+                e.appendChild(n);
+            }
+        } catch (e) {
+            console.log(e);
+            if (window.tw_errors) {
+                console.log(window.tw_errors);
+            }
+        }
+    </script>
+
+@endsection

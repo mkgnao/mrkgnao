285782c31d46d4d147a371adbb7b63b339ae2c98
ddyok <mkgnao@yandex.com>
2016-01-08 22:30:39 +0100

    comment

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -139,7 +139,7 @@
 <footer class="HolyGrail-footer">
     <div class="Footer">
         <div class="Footer-credits">
-            <span class="Footer-credit">copyright © 2016 mkgnao | contact@mkgnao.co</span>
+            <span class="Footer-credit">copyright © 2016 | contact@mkgnao.co</span>
         </div>
     </div>
 </footer>

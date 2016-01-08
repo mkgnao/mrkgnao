97ff605494325910dc699f83e4811db2c7f24817
ddyok <mkgnao@yandex.com>
2016-01-09 00:03:29 +0100

    comment

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,13 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
-    <form id="login-form" role="form" method="POST" action="{{ url('/login') }}" value="{{ csrf_token() }}">
+    <form role="form" method="POST" action="{{ url('/login') }}" value="{{ csrf_token() }}">
         {!! csrf_field() !!}
 
         <div class="Container">
             <div class="Grid Grid--guttersLg Grid--full med-Grid--fit">
 
-                <div-- class="Grid-cell">
+                <div class="Grid-cell">
                     <div class="InputAddOn">
                         <!--span class="InputAddOn-item">email</span-->
 
@@ -32,9 +32,14 @@
                             <input type="checkbox" class="InputAddOn-item-checkbox" name="remember">
                         </div>
                     </div-->
-                    <a id="form-submit" class="Header-button Button Button--action Button--wide">
-                        <span class="Header-button-a">login</span>
-                    </a>
+                    <div class="InputAddOn">
+                        <button class="InputAddOn-button-login" type="submit">
+                            login
+                        </button>
+                        <!--div class="InputAddOn-item-checkbox">
+                            <input type="checkbox" class="InputAddOn-item-checkbox" name="remember">
+                        </div-->
+                    </div>
                     <!--div class="InputAddOn">
                         <a class="InputAddOn-item-link" href="{{ url('/password/reset') }}">reset</a>
                     </div-->
@@ -42,10 +47,4 @@
             </div>
         </div>
     </form>
-    <script>
-        var form = document.getElementById("login-form");
-        document.getElementById("form-submit").addEventListener("click", function () {
-            form.submit();
-        });
-    </script>
 @endsection

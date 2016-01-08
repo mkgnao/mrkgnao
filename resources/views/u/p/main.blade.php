3d9b08f2a317f086fe7c760ec1fbb86fa37cfdbf
ddyok <mkgnao@yandex.com>
2016-01-08 17:16:36 +0100

    comment

--- resources/views/u/p/main.blade.php
+++ resources/views/u/p/main.blade.php
@@ -22,15 +22,17 @@
             } else {
                 var e = document.getElementById("twcontent");
                 var n = document.createElement("div");
-                n.id = "twnotfound";
-                n.innerHTML = "not on teamwork";
+                n.id = "error";
+                if (window.tw_errors) {
+                    n.innerHTML = console.log(window.tw_errors);
+                } else {
+                    n.innerHTML = "oops";
+                }
                 e.appendChild(n);
             }
         } catch (e) {
             console.log(e);
-            if (window.tw_errors) {
-                console.log(window.tw_errors);
-            }
+
         }
     </script>
 

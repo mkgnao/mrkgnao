2b3ab5d43ea6cfa55d852b834ea0f68f497945f3
ddyok <mkgnao@yandex.com>
2016-01-09 10:18:11 +0100

    comment

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,8 +20,7 @@
     <link href="/css/twtable.css" rel="stylesheet" type="text/css">
 
     <script src="/js/main.js"></script>
-    <script src="/js/prettyprint.js"></script>
-
+    <script src="/js/jp.js"></script>
 
     <script>
         function addLoadEvent(func) {

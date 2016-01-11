9a90b9409099ab60fbbec479665121060b4c3285
ddyok <mkgnao@yandex.com>
2016-01-11 23:47:23 +0100

    comment

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,26 @@
 @extends('layouts.app')
 
 @section('content')
+    writers
+
+    <table>
+        <tr>
+            <td>Felix Laubscher</td>
+        </tr>
+        <tr>
+            <td>PhD candidate at UdK Berlin. Camera operator.
+            </td>
+        </tr>
+
+        <tr>
+            <td>Emma Simon</td>
+        </tr>
+        <tr>
+            <td>Camera operator and director. Studied film at dffb Berlin. Winner of the 2012 Bavarian Documentary Film Award.
+            </td>
+        </tr>
+
+    </table>
+
+
 @endsection

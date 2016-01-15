80241187fd66dced9b5d5ec5ca4157af6210e450
ddyok <mkgnao@yandex.com>
2016-01-16 00:09:56 +0100

    comment

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,6 +1,9 @@
 <!DOCTYPE html>
 <html lang="en" xmlns="http://www.w3.org/1999/html">
-    @include('partials.headall')
+<!-- TOP -->
+@include('partials.headall')
 
-    @include('partials.mainbody')
+@yield('partials.mainbody')
+
+<!-- TOP -->
 </html>

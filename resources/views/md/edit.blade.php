9b6627266793a83fbf9d626d873cf63698f0417f
ddyok <mkgnao@yandex.com>
2016-01-16 20:16:24 +0100

    messing around...

--- resources/views/md/edit.blade.php
+++ resources/views/md/edit.blade.php
@@ -6,7 +6,7 @@
     <div class="Container">
         <div class="Grid Grid--guttersLg Grid--full med-Grid--fit">
 
-            {!!Form::model($mdContent,array('method' => 'Patch', 'route' => array('md/update/'.$mdContent->id))) !!}
+            {!!Form::model($mdContent,array('method' => 'POST', 'route' => array('md/update', $mdContent->id))) !!}
             <input type="hidden" name="_method" value="PATCH">
 
             <div class="Grid-cell">

c0247263125f35c18b170321e059ee0ec96027e4
ddyok <mkgnao@yandex.com>
2016-01-16 08:42:26 +0100

    messing around...

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -2,7 +2,9 @@
 
 namespace App\Http;
 
-use Illuminate\Support\Facades\Log as Log;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Routing\Route;
+use Illuminate\View\View;
 
 /*
 |--------------------------------------------------------------------------

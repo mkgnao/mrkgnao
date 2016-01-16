8d17bf11d85173600c9e2f7f554134d96f9511a3
ddyok <mkgnao@yandex.com>
2016-01-16 06:29:46 +0100

    messing around...

--- resources/views/partials/appfronttopnav.blade.php
+++ resources/views/partials/appfronttopnav.blade.php
@@ -1,5 +1,5 @@
-<a class="InputAddOn-button-login" href="{{ url('/about') }}">about</a>
-<a class="InputAddOn-button-login" href="{{ url('/writers') }}">writers</a>
-<a class="InputAddOn-button-login" href="{{ url('/partners') }}">partners</a>
-<a class="InputAddOn-button-login" href="{{ url('/projects') }}">projects</a>
-<a class="InputAddOn-button-login" href="{{ url('/internships') }}">internships</a>
+<a class="InputAddOn-button-fronttopnav" href="{{ url('/about') }}">about</a>
+<a class="InputAddOn-button-fronttopnav" href="{{ url('/writers') }}">writers</a>
+<a class="InputAddOn-button-fronttopnav" href="{{ url('/partners') }}">partners</a>
+<a class="InputAddOn-button-fronttopnav" href="{{ url('/projects') }}">projects</a>
+<a class="InputAddOn-button-fronttopnav" href="{{ url('/internships') }}">internships</a>

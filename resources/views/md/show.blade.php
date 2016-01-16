bc6363a446a16f5645871befc6e7a2f662c25591
ddyok <mkgnao@yandex.com>
2016-01-16 17:58:44 +0100

    messing around...

--- resources/views/md/show.blade.php
+++ resources/views/md/show.blade.php
@@ -3,19 +3,22 @@
 @extends('layouts.app')
 
 @section('content')
+        {{ dd($mdContent->name) }}
 
-{!! Form::model($mdContent, array('route' => array('md.update', $mdContent->name)) !!}
+    {{--
+    {!! Form::model($mdContent, array('route' => array('md.update', $mdContent->name)) !!}
 
-        <div class="form-group">
-            {!! Form::label('content', 'Content:') !!}
-            {!! Form::text('content', null, ['class' => 'form-control']) !!}
-        </div>
+            <div class="form-group">
+                {!! Form::label('content', 'Content:') !!}
+                {!! Form::text('content', null, ['class' => 'form-control']) !!}
+            </div>
 
-        <div class="form-group">
-            {!! Form::submit('submit', ['class' => 'form-control']) !!}
-        </div>
+            <div class="form-group">
+                {!! Form::submit('submit', ['class' => 'form-control']) !!}
+            </div>
 
-{!! Form::close() !!}
+    {!! Form::close() !!}
+    --}}
 
-@endsection
-        <!-- END MD/SHOW -->
+    @endsection
+            <!-- END MD/SHOW -->

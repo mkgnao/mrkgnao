4ac6dae027122a6f79bbd674cb0a4cf3a18c03a5
ddyok <mkgnao@yandex.com>
2016-01-16 19:43:18 +0100

    messing around...

--- resources/views/md/edit.blade.php
+++ resources/views/md/edit.blade.php
@@ -6,7 +6,7 @@
     <div class="Container">
         <div class="Grid Grid--guttersLg Grid--full med-Grid--fit">
 
-            {!!Form::model($mdContent,array('method' =>' Patch', 'route' => array('md/update', $mdContent->id))) !!}
+            {!!Form::model($mdContent,array('method' =>' Patch', 'route' => array('md/edit', $mdContent->id))) !!}
 
             <div class="Grid-cell">
                 <div class="InputAddOn">

475e2d4f1723021cae21296efa31cdda94b3d6d4
ddyok <mkgnao@yandex.com>
2016-01-16 18:45:58 +0100

    messing around...

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -88,7 +88,7 @@ Route::group(['middleware' => 'web'], function ($view) {
         return view('mdcontent', array('name' => 'welcome'));
     });
 
-    Route::post('md.update', array('before' => 'csrf', 'uses' => 'MdController@update'));
+    Route::put('md.update', array('before' => 'csrf', 'uses' => 'MdController@update'));
 
     Route::get('md/{id}', 'MdController@show');
 

b9e549791e4d965b78377a1c687333f05d1c387b
ddyok <mkgnao@yandex.com>
2016-01-16 05:43:36 +0100

    messing around...

--- resources/views/partials/appfooter.blade.php
+++ resources/views/partials/appfooter.blade.php
@@ -4,11 +4,11 @@
         <div class="Footer-credits">
             <span class="Footer-credit">copyright © 2016</span>
         </div>
-    </div>
-    <div class="Footer-right">
-        @if (Auth::guest())
-            <a class="InputAddOn-button-login" href="{{ url('/login') }}">login</a>
-        @endif
+        <div class="Footer-right">
+            @if (Auth::guest())
+                <a class="InputAddOn-button-login" href="{{ url('/login') }}">login</a>
+            @endif
+        </div>
     </div>
 </footer>
 <!-- END LAYOUTS/PARTIALS/APPFOOTER -->

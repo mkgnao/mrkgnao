14168cef801d3c6eba4db34442bc0e5f320788d6
ddyok <mkgnao@yandex.com>
2016-01-18 21:31:24 +0100

    BLA

--- resources/views/partials/maincontent.blade.php
+++ resources/views/partials/maincontent.blade.php
@@ -4,12 +4,10 @@
         <article class="HolyGrail-content" class="u-textCenter">
             <div class="center-title">the category</div>
 
-            <!--
-            <div><a class="center-title" href="{{ url('/writers') }}">writers</a></div>
-            <div><a class="center-title" href="{{ url('/projects') }}">projects</a></div>
-            <div><a class="center-title" href="{{ url('/partners') }}">partners</a></div>
-            <div><a class="center-title" href="{{ url('/contact') }}">contact</a></div>
-            -->
+            <div><a class="center-links" href="{{ url('/writers') }}">writers</a></div>
+            <div><a class="center-links" href="{{ url('/projects') }}">projects</a></div>
+            <div><a class="center-links" href="{{ url('/partners') }}">partners</a></div>
+            <div><a class="center-links" href="{{ url('/contact') }}">contact</a></div>
 
             @yield('content')
         </article>

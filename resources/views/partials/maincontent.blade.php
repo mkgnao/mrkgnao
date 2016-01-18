9860d6cccad3c1ee75cd6acdb50ac0b1b05b328a
ddyok <mkgnao@yandex.com>
2016-01-18 23:01:33 +0100

    BLA

--- resources/views/partials/maincontent.blade.php
+++ resources/views/partials/maincontent.blade.php
@@ -3,15 +3,26 @@
     <main class="HolyGrail-body">
         <article class="HolyGrail-content" class="u-textCenter">
 
+            <div class="Container">
+                <div class="Grid Grid--guttersLg Grid--full med-Grid--fit">
+                    <div class="Grid-cell">
+                        <div class="center-links">
 
-            <div class="center-links">
+                            <a class="center-link" href="{{ url('/writers') }}">writers</a>
+                            <a class="center-link" href="{{ url('/projects') }}">projects</a>
+                            <a class="center-link" href="{{ url('/partners') }}">partners</a>
+                            <a class="center-link" href="{{ url('/contact') }}">contact</a>
 
-                <a class="center-link" href="{{ url('/writers') }}">writers</a>
-                <a class="center-link" href="{{ url('/projects') }}">projects</a>
-                <a class="center-link" href="{{ url('/partners') }}">partners</a>
-                <a class="center-link" href="{{ url('/contact') }}">contact</a>
+                        </div>
 
+                        <div class="writersList">
+                            {!! $markdownService->get($name) !!}
+                        </div>
+                    </div>
+                </div>
             </div>
+
+
 
             @yield('content')
         </article>

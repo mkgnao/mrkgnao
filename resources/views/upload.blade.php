5c79fc1705b660d9c241b067d8da1ff403487a2d
ddyok <mkgnao@yandex.com>
2016-01-18 04:38:28 +0100

    BLA

--- resources/views/upload.blade.php
+++ resources/views/upload.blade.php
@@ -5,6 +5,6 @@
     <form method="post" enctype="multipart/form-data" name="{{$name}}" action="/upload/{{$name}}">
         <input type="hidden" name="MAX_FILE_SIZE" value="20000">
         <input name="{{$name}}" type="file" id="{{$name}}">
-        <input name="upload" type="submit" value="upload">
+        <input name="{{$name}}" type="submit" value="{{$name}}">
     </form>
 @endsection

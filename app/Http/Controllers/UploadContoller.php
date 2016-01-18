8c8a6b9f83d20338a30b2b68c6e7a1f0f6827ce5
ddyok <mkgnao@yandex.com>
2016-01-18 17:21:19 +0100

    BLA

--- app/Http/Controllers/UploadContoller.php
+++ app/Http/Controllers/UploadContoller.php
@@ -13,7 +13,7 @@ class UploadController extends Controller
     private static function save(Request $request, $mdId)
     {
 
-        \Log::info('writers: '.print_r($request->has('writers')));
+        \Log::info('writers: '.$request->has('writers'));
 
         $urlComp = explode("/", parse_url($request->server('HTTP_REFERER'), PHP_URL_PATH));
 

92a0d411bbadb935697982bab4b8333e304a0561
ddyok <mkgnao@yandex.com>
2016-01-18 23:31:06 +0100

    BLA

--- resources/views/partials/appfronttopnav.blade.php
+++ resources/views/partials/appfronttopnav.blade.php
@@ -1,1 +1,5 @@
-<div class="center-title">the category</div>
+<div class="Container">
+    <div class="Grid Grid--guttersLg Grid--full med-Grid--fit">
+        <div class="Grid-cell">
+            <div class="center-title">the category</div>
+            </div></div></div>
--- resources/views/partials/maincontent.blade.php
+++ resources/views/partials/maincontent.blade.php
@@ -6,13 +6,10 @@
             <div class="Container">
                 <div class="Grid Grid--guttersLg Grid--full med-Grid--fit">
                     <div class="Grid-cell">
-                        <div class="center-links">
-
                             <a class="center-link" href="{{ url('/writers') }}">writers</a>
                             <a class="center-link" href="{{ url('/projects') }}">projects</a>
                             <a class="center-link" href="{{ url('/partners') }}">partners</a>
                             <a class="center-link" href="{{ url('/contact') }}">contact</a>
-
                         </div>
 
                         <div class="writersList">
@@ -20,7 +17,6 @@
                         </div>
                     </div>
                 </div>
-            </div>
 
 
 

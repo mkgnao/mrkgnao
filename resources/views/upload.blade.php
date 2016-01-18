69d1849828487e774bde7da6bfbee2d3fc5cb1ab
ddyok <mkgnao@yandex.com>
2016-01-18 04:36:29 +0100

    BLA

--- resources/views/upload.blade.php
+++ resources/views/upload.blade.php
@@ -2,7 +2,7 @@
 @extends('layouts.app')
 
 @section('content')
-    <form method="post" enctype="multipart/form-data" action="/upload/{{$name}}">
+    <form method="post" enctype="multipart/form-data" name="{{$name}}" action="/upload/{{$name}}">
         <input type="hidden" name="MAX_FILE_SIZE" value="20000">
         <input name="{{$name}}" type="file" id="{{$name}}">
         <input name="upload" type="submit" value="upload">

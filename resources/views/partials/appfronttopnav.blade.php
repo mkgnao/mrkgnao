dc7e22c7d4681ca71faae62ddfbee6cc4b5913ef
ddyok <mkgnao@yandex.com>
2016-01-20 00:28:55 +0100

    bla

--- resources/views/partials/appfronttopnav.blade.php
+++ resources/views/partials/appfronttopnav.blade.php
@@ -1,32 +1,34 @@
-<nav role='navigation' class="main-nav" id="main-nav">
-    <ul id="main-nav-list">
-        <li>
-            <a href="#">
-                <div>
-                    writers
-                </div>
-            </a>
-        </li>
-        <li>
-            <a href="#">
-                <div>
-                    projects
-                </div>
-            </a>
-        </li>
-        <li>
-            <a href="#">
-                <div>
-                    partners
-                </div>
-            </a>
-        </li>
-        <li>
-            <a href="#">
-                <div>
-                    contact
-                </div>
-            </a>
-        </li>
-    </ul>
-</nav>
+<div class="topnav">
+    <nav role='navigation' class="main-nav" id="main-nav">
+        <ul id="main-nav-list">
+            <li>
+                <a href="#">
+                    <div>
+                        writers
+                    </div>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <div>
+                        projects
+                    </div>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <div>
+                        partners
+                    </div>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <div>
+                        contact
+                    </div>
+                </a>
+            </li>
+        </ul>
+    </nav>
+</div>

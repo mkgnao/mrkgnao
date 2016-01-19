0cacd051cb9b9ac7cce458b35e580285092491d2
ddyok <mkgnao@yandex.com>
2016-01-19 21:58:03 +0100

    bla

--- resources/views/partials/appfooter.blade.php
+++ resources/views/partials/appfooter.blade.php
@@ -2,11 +2,8 @@
 <footer class="Site-footer">
     <div class="Footer">
         <div class="Footer-credits">
-            <span class="Footer-credit">copyright © 2016 the category | <a class="Footer-credit"
-                                                                           href="{!! url('/about') !!}">about</a>
-                @if (Auth::guest())
-                    | <a class="Footer-credit" href="{!! url('/login') !!}">login</a>
-                @endif
+            <span class="Footer-credit">copyright © 2016 the category
+
             </span>
         </div>
     </div>

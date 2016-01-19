04094c3e667978454bc6d88ea2a641157c5259e3
ddyok <mkgnao@yandex.com>
2016-01-19 23:42:22 +0100

    bla

--- resources/views/partials/maincontent.blade.php
+++ resources/views/partials/maincontent.blade.php
@@ -1,22 +1,23 @@
 <!-- BEGIN LAYOUTS/PARTIALS/MAINCONTENT -->
-<div class="scrollable">
-    <main class="HolyGrail-body">
-        <article class="HolyGrail-content" class="u-textCenter">
-            <div class="Container">
-                <div class="Grid Grid--guttersLg Grid--full med-Grid--fit">
-                    <div class="Grid-cell">
-                        <div class="center-title">THE CATEGORY</div>
-                        <div class="center-tagline">The writing company.</div>
-                    </div>
+
+<main class="HolyGrail-body">
+    <article class="HolyGrail-content" class="u-textCenter">
+        <div class="Container">
+            <div class="Grid Grid--guttersLg Grid--full med-Grid--fit">
+                <div class="Grid-cell">
+                    <div class="center-title">THE CATEGORY</div>
+                    <div class="center-tagline">The writing company.</div>
                 </div>
             </div>
-
+        </div>
+        <div class="scrollable">
             @yield('content')
-        </article>
-        <nav class="HolyGrail-nav u-textCenter">
-        </nav>
-        <aside class="HolyGrail-ads u-textCenter">
-        </aside>
-    </main>
-</div>
-    <!-- END LAYOUTS/PARTIALS/MAINCONTENT -->
+        </div>
+    </article>
+    <nav class="HolyGrail-nav u-textCenter">
+    </nav>
+    <aside class="HolyGrail-ads u-textCenter">
+    </aside>
+</main>
+
+<!-- END LAYOUTS/PARTIALS/MAINCONTENT -->

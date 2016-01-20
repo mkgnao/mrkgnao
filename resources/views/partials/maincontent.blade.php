4fd7bab2785991205fc3ff9bf3d81dd545665704
ddyok <mkgnao@yandex.com>
2016-01-20 02:04:20 +0100

    bla

--- resources/views/partials/maincontent.blade.php
+++ resources/views/partials/maincontent.blade.php
@@ -1,6 +1,6 @@
 <!-- BEGIN LAYOUTS/PARTIALS/MAINCONTENT -->
 
-<main class="HolyGrail-body">
+<main class="HolyGrail-body opaque">
     <article class="HolyGrail-content" class="u-textCenter">
         <div class="Container">
             <div class="Grid Grid--guttersLg Grid--full med-Grid--fit">

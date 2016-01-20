0cfee0fe9e26d9290dda8a0a1055d393d639b167
ddyok <mkgnao@yandex.com>
2016-01-20 01:23:30 +0100

    bla

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,50 @@
 @include('partials.mainbody')
 </body>
 
+<div id="Modal-flex-container-logout" class="Modal-flex-container-hidden">
+    <div id="Modal-row">
+        <div class="Modal-flex-item">
+            <form role="form" method="POST" action="{!! url('/login') !!}" value="{!! csrf_token() !!}">
+                {!! csrf_field() !!}
+
+                <div class="Container">
+                    <div class="Grid Grid--guttersLg Grid--full med-Grid--fit">
+                        <div class="Grid-cell">
+                            <div class="InputAddOn">
+                                <input type="email" class="InputAddOn-field" name="email"
+                                       value="{!! old('email') !!}" placeholder="email">
+
+                                @if ($errors->has('email'))
+                                    {!! $errors->first('email') !!}
+                                @endif
+                            </div>
+                            <div class="InputAddOn">
+                                <input type="password" class="InputAddOn-field" name="password" placeholder="password">
+
+                                @if ($errors->has('password'))
+                                    {!! $errors->first('password') !!}
+                                @endif
+                            </div>
+                            <div class="InputAddOn">
+                                <button class="InputAddOn-button-login" type="submit">
+                                    login
+                                </button>
+                                <!--div class="InputAddOn-item-checkbox">
+                                    <input type="checkbox" class="InputAddOn-item-checkbox" name="remember">
+                                </div-->
+                            </div>
+                            <!--div class="InputAddOn">
+                        <a class="InputAddOn-item-link" href="{!! url('/password/reset') !!}">reset</a>
+                    </div-->
+                        </div>
+                    </div>
+                </div>
+            </form>
+            <a id="logoutModalLogout" href="{!! url('/logout') !!}"
+               class="Modal-Button Modal-Button--action Modal-Button--wide">bla</a>
+        </div>
+    </div>
+</div>
 
 <!-- END LAYOUTS/APP -->
 </html>

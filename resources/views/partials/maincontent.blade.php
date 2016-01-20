7f0cab2b3378d702fd32b9a0960ed200a8473553
ddyok <mkgnao@yandex.com>
2016-01-20 03:53:51 +0100

    bla

--- resources/views/partials/maincontent.blade.php
+++ resources/views/partials/maincontent.blade.php
@@ -8,15 +8,130 @@
                     <!-- should be in motherfucking md file -->
                     <div class="center-title">THE CATEGORY</div>
                     <div id="center-tagline" class="center-tagline">
-                        <div class="center-tagline-content" ><div>The writing company.</div></div>
-                        <div class="center-tagline-content" ><div>We write like motherfuckers.</div></div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="scrollable">
-            @yield('content')
-        </div>
+                        <div class="center-tagline-content">
+                            <div>The writing company.</div>
+                        </div>
+                        <div class="center-tagline-content">
+                            <div>We write like motherfuckers.</div>
+                        </div>
+                        <div class="center-tagline-content">
+                            <div>You lady poofta.</div>
+                        </div>
+                        <div class="center-tagline-content">
+                            <div>The Wild Swans at Coole,</div>
+                        </div>
+                        <div class="center-tagline-content">
+                            <div>By William Butler Yeats,</div>
+                        </div>
+                        <div class="center-tagline-content">
+                            <div>Is an overrated poem.</div>
+                        </div>
+                        <div class="center-tagline-content">
+                            <div>Yet,</div>
+                        </div>
+                        <div class="center-tagline-content">
+                            <div>Consider:</div>
+                        </div>
+                        <div class="center-tagline-content">
+                            <div>The trees are in their autumn beauty,</div>
+                        </div>
+                        <div class="center-tagline-content">
+                            <div>The woodland paths are dry,</div>
+                        </div>
+                        <div class="center-tagline-content">
+                            <div>Under the October twilight the water</div>
+                        </div>
+                        <div class="center-tagline-content">
+                            <div>Mirrors a still sky;</div>
+                        </div>
+                        <div class="center-tagline-content">
+                            <div>Upon the brimming water among the stones</div>
+                        </div>
+                        <div class="center-tagline-content">
+                            <div>Are nine-and-fifty swans.</div>
+                        </div>
+
+                        <div class="center-tagline-content">
+                            <div>AThe nineteenth autumn has come upon me</div>
+                        </div>
+                        <div class="center-tagline-content">
+                            <div>ASince I first made my count;</div>
+                        </div>
+                        <div class="center-tagline-content">
+                            <div>AI saw, before I had well finished,</div>
+                        </div>
+                        <div class="center-tagline-content">
+                            <div>AAll suddenly mount</div>
+                        </div>
+                        <div class="center-tagline-content">
+                            <div>AAnd scatter wheeling in great broken rings</div>
+                        </div>
+                        <div class="center-tagline-content">
+                            <div>AUpon their clamorous wings.</div>
+                        </div>
+
+                        <div class="center-tagline-content">
+                            <div>AI have looked upon those brilliant creatures,</div>
+                        </div>
+                        <div class="center-tagline-content">
+                            <div>AAnd now my heart is sore.</div>
+                        </div>
+                        <div class="center-tagline-content">
+                            <div>AAll's changed since I, hearing at twilight,</div>
+                        </div>
+                        <div class="center-tagline-content">
+                            <div>AThe first time on this shore,</div>
+                        </div>
+                        <div class="center-tagline-content">
+                            <div>AThe bell-beat of their wings above my head,</div>
+                        </div>
+                        <div class="center-tagline-content">
+                            <div>ATrod with a lighter tread.</div>
+                        </div>
+
+                        <div class="center-tagline-content">
+                            <div>AUnwearied still, lover by lover,</div>
+                        </div>
+                        <div class="center-tagline-content">
+                            <div>They paddle in the cold</div>
+                        </div>
+                        <div class="center-tagline-content">
+                            <div>Companionable streams or climb the air;</div>
+                        </div>
+                        <div class="center-tagline-content">
+                            <div>Their hearts have not grown old;</div>
+                        </div>
+                        <div class="center-tagline-content">
+                            <div>Passion or conquest, wander where they will,</div>
+                        </div>
+                        <div class="center-tagline-content">
+                            <div>Attend upon them still.</div>
+                        </div>
+
+                        <div class="center-tagline-content">
+                            <div>But now they drift on the still water,</div>
+                        </div>
+                        <div class="center-tagline-content">
+                            <div> Mysterious, beautiful;</div>
+                        </div>
+                        <div class="center-tagline-content">
+                            <div>Among what rushes will they build,</div>
+                        </div>
+
+                        <div class="center-tagline-content">
+                            <div>By what lake's edge or pool</div>
+                        </div>
+
+                        <div class="center-tagline-content">
+                            <div>Delight men's eyes when I awake some day</div>
+                        </div>
+                        <div class="center-tagline-content">
+                            <div>To find they have flown away?</div>
+                        </div>
+
+                        <div class="scrollable">
+                            @yield('content')
+                        </div>
     </article>
     <nav class="HolyGrail-nav u-textCenter">
     </nav>

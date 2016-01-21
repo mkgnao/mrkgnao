681bc8c7cdeb77d3e56121aa73d6465e1f3a3cc7
ddyok <mkgnao@yandex.com>
2016-01-21 18:46:02 +0100

    bla

--- resources/views/partials/appfooter.blade.php
+++ resources/views/partials/appfooter.blade.php
@@ -2,7 +2,7 @@
 <footer class="Site-footer opaque">
     <div class="Footer">
         <div class="Footer-credits">
-            <span class="Footer-credit">copyright &copy; 2016 | writers@thecategory.com</span>
+            <span class="Footer-credit">copyright &copy; 2016 | <a href="writers@thecategory.com">writers@thecategory.com</a></span>
         </div>
     </div>
 </footer>

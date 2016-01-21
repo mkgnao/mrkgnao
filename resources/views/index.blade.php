ba9ff06687a699ff8cb7028cee293c61c02cbadf
ddyok <mkgnao@yandex.com>
2016-01-21 19:20:14 +0100

    bla

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -17,124 +17,16 @@
                                 <div>The writing company.</div>
                             </div>
                             <div class="center-tagline-content">
-                                <div>We write like motherfuckers.</div>
+                                <div>Time for you and time for me,</div>
                             </div>
                             <div class="center-tagline-content">
-                                <div>Wait,</div>
+                                <div>And time yet for a hundred indecisions,</div>
                             </div>
                             <div class="center-tagline-content">
-                                <div>Also,</div>
+                                <div>And for a hundred visions and revisions,</div>
                             </div>
                             <div class="center-tagline-content">
-                                <div>The Wild Swans at Coole,</div>
-                            </div>
-                            <div class="center-tagline-content">
-                                <div>By William Butler Yeats,</div>
-                            </div>
-                            <div class="center-tagline-content">
-                                <div>Is still a good poem,</div>
-                            </div>
-                            <div class="center-tagline-content">
-                                <div>(Even though it's &quot;required reading&quot;).</div>
-                            </div>
-                            <div class="center-tagline-content">
-                                <div>Consider:</div>
-                            </div>
-                            <div class="center-tagline-content">
-                                <div>The trees are in their autumn beauty,</div>
-                            </div>
-                            <div class="center-tagline-content">
-                                <div>The woodland paths are dry,</div>
-                            </div>
-                            <div class="center-tagline-content">
-                                <div>Under the October twilight the water</div>
-                            </div>
-                            <div class="center-tagline-content">
-                                <div>Mirrors a still sky;</div>
-                            </div>
-                            <div class="center-tagline-content">
-                                <div>Upon the brimming water among the stones</div>
-                            </div>
-                            <div class="center-tagline-content">
-                                <div>Are nine-and-fifty swans.</div>
-                            </div>
-
-                            <div class="center-tagline-content">
-                                <div>The nineteenth autumn has come upon me</div>
-                            </div>
-                            <div class="center-tagline-content">
-                                <div>Since I first made my count;</div>
-                            </div>
-                            <div class="center-tagline-content">
-                                <div>I saw, before I had well finished,</div>
-                            </div>
-                            <div class="center-tagline-content">
-                                <div>All suddenly mount</div>
-                            </div>
-                            <div class="center-tagline-content">
-                                <div>And scatter wheeling in great broken rings</div>
-                            </div>
-                            <div class="center-tagline-content">
-                                <div>Upon their clamorous wings.</div>
-                            </div>
-
-                            <div class="center-tagline-content">
-                                <div>I have looked upon those brilliant creatures,</div>
-                            </div>
-                            <div class="center-tagline-content">
-                                <div>And now my heart is sore.</div>
-                            </div>
-                            <div class="center-tagline-content">
-                                <div>All's changed since I, hearing at twilight,</div>
-                            </div>
-                            <div class="center-tagline-content">
-                                <div>The first time on this shore,</div>
-                            </div>
-                            <div class="center-tagline-content">
-                                <div>The bell-beat of their wings above my head,</div>
-                            </div>
-                            <div class="center-tagline-content">
-                                <div>Trod with a lighter tread.</div>
-                            </div>
-
-                            <div class="center-tagline-content">
-                                <div>Unwearied still, lover by lover,</div>
-                            </div>
-                            <div class="center-tagline-content">
-                                <div>They paddle in the cold</div>
-                            </div>
-                            <div class="center-tagline-content">
-                                <div>Companionable streams or climb the air;</div>
-                            </div>
-                            <div class="center-tagline-content">
-                                <div>Their hearts have not grown old;</div>
-                            </div>
-                            <div class="center-tagline-content">
-                                <div>Passion or conquest, wander where they will,</div>
-                            </div>
-                            <div class="center-tagline-content">
-                                <div>Attend upon them still.</div>
-                            </div>
-
-                            <div class="center-tagline-content">
-                                <div>But now they drift on the still water,</div>
-                            </div>
-                            <div class="center-tagline-content">
-                                <div> Mysterious, beautiful;</div>
-                            </div>
-                            <div class="center-tagline-content">
-                                <div>Among what rushes will they build,</div>
-                            </div>
-
-                            <div class="center-tagline-content">
-                                <div>By what lake's edge or pool</div>
-                            </div>
-
-                            <div class="center-tagline-content">
-                                <div>Delight men's eyes when I awake some day</div>
-                            </div>
-                            <div class="center-tagline-content">
-                                <div>To find they have flown away?</div>
+                                <div>Before the taking of a toast and tea.</div>
                             </div>
                         </div>
                     </div>
